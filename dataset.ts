bade8433fc055ce34d31390fb0ec74a82afbf196
MadhuSudhanGowda97 <127107178+MadhuSudhanGowda97@users.noreply.github.com>
2023-03-17 16:20:34 +0530

    Add files via upload

--- dataset.ts
+++ dataset.ts
@@ -4,7 +4,7 @@ var app: Application = express();
 var port =2023
 import { Application } from 'express';
 // const db = require('./datasetqueries')
-import { createUser, deleteUser, getUserById, getUsers, updateUser } from './datasetqueries';
+import { createUser, deleteUser, getUserById, getUsers, partialupdateUser, updateUser } from './datasetqueries';
 app.use(express.json())
 
 
@@ -20,9 +20,19 @@ app.post('/dataset1/create',createUser)
 //Updated data
 app.put('/dataset1/update/:id',updateUser)
 
+//partially updating data
+app.patch('/dataset1/partupdate/:id',partialupdateUser)
+
 //Deleting data
 app.delete('/dataset1/delete/:id',deleteUser)
-        
+
+app.all('*',(req:any,res:any)=>{
+  res.status(404).send({
+    "status_code": 404.1,
+    "error_code": "Page Not Found"
+  })
+})
+
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
   })
--- datasetqueries.ts
+++ datasetqueries.ts
@@ -2,6 +2,7 @@ var Pool = require('pg').Pool
 var pool = new Pool({
   user: 'postgres',
   host: 'localhost',
+  //'host.docker.internal' if we want to connect in docker
   database: 'postgres',
   password: '12345',
   port: 5432
@@ -18,13 +19,26 @@ interface Dbrecord {
   updated_date: string
 }
 import { Request, Response } from "express";
-pool.connect(function (err: any,res:any) {
-  if (err) {console.error("Bad connection");
-  throw err
+pool.connect(function (err: any, res: any) {
+  if (err) {
+    console.error("Bad connection");
+    console.log(err)
+    try {
+      throw err
+    }
+    catch (err) {
+      // console.error({
+      //   "error code":502,
+      //   "error message":"Bad gateway"
+      // })
+    }
   }
-  else{
-  console.log("Connected!");}
+  else {
+    console.log("Connected!");
+  }
 });
+
+//get all users
 var getUsers = async (req: Request, res: Response) => {
   var aclient = await pool.query('select * from datasets order by id;')
   if (aclient.rows.length === 0) {
@@ -36,9 +50,10 @@ var getUsers = async (req: Request, res: Response) => {
     })
   }
   else {
-    res.send(aclient.rows)
+    res.json(aclient.rows)
   }
 }
+//get user by id
 var getUserById = async (req: Request, res: Response) => {
   var id = parseInt(req.params.id)
 
@@ -56,7 +71,7 @@ var getUserById = async (req: Request, res: Response) => {
     }
   })
 }
-
+//create user
 var createUser = async (req: Request, res: Response) => {
   var now = new Date()
   var { id, data_schema, router_config, status, created_by, updated_by, created_dated, updated_date }: Dbrecord = req.body
@@ -67,15 +82,16 @@ var createUser = async (req: Request, res: Response) => {
         if (!error) {
           res.status(201).send({
             "status_code": 201,
-            "success_message":"The record is successfully created"
+            "success_message": "The record is successfully created"
           })
         }
         else {
           res.status(400).send({
-          "status_code": 400,
-          "reason_phrase": "The requested operation failed because there is no id in the input.",
-          "error_code": "Bad Request",
-          "error_message": "Id is missing"})
+            "status_code": 400,
+            "reason_phrase": "The requested operation failed because there is missing data in the input.",
+            "error_code": "Bad Request",
+            "error_message": "Data is incomplete"
+          })
         }
       })
   }
@@ -90,39 +106,78 @@ var createUser = async (req: Request, res: Response) => {
 }
 
 
-var updateUser = async(req:any,res:any)=>
-  {
-    var now = new Date()
-     var id =parseInt(req.params.id)
-      var recExists = await pool.query(`select * from datasets where id = '${id}'`)
-      var {data_schema, router_config, status, updated_by }:Dbrecord=req.body
-      if (recExists.rows.length != 0) {
-      pool.query(`UPDATE datasets set data_schema=$1,router_config=$2,status=$3,updated_by=$4,updated_date=$5 where id =$6 ;`,
-           [data_schema, router_config, status, updated_by, now,id ],(error:any, result:any) => 
-           {
-              if (!error) {
-                res.status(200).send({
-                  "status_code": 200,
-                  "success_message":"The record is successfully updated"
-              })
-              }
-              else{
-                res.status(400).send({
-                  "status_code": 400,
-                  "reason_phrase": "Null values found.",
-                  "error_code": "Bad Request",
-                  "error_message": "insufficient data"})
-              }
-            })}
-              else{
-                res.status(404).send({
-                  "status_code": 404,
-                  "reason_phrase": "The requested operation failed because a resource associated with the request could not be found.",
-                  "error_code": "Not_found",
-                  "error_message": "The data you requested could not be found in Database"
-              })
-            }
-            }
+var updateUser = async (req: any, res: any) => {
+  var now = new Date()
+  var id = parseInt(req.params.id)
+  var recExists = await pool.query(`select * from datasets where id = '${id}'`)
+  var { data_schema, router_config, status, updated_by }: Dbrecord = req.body
+  if (recExists.rows.length != 0) {
+    pool.query(`UPDATE datasets set data_schema=$1,router_config=$2,status=$3,updated_by=$4,updated_date=$5 where id =$6 ;`,
+      [data_schema, router_config, status, updated_by, now, id], (error: any, result: any) => {
+      if (!error) {
+        res.status(200).send({
+          "status_code": 200,
+          "success_message": "The record is successfully updated"
+        })
+      }
+      else {
+        res.status(400).send({
+          "status_code": 400,
+          "reason_phrase": "Null values found.",
+          "error_code": "Bad Request",
+          "error_message": "insufficient data"
+        })
+      }
+    })
+  }
+  else {
+    res.status(404).send({
+      "status_code": 404,
+      "reason_phrase": "The requested operation failed because a resource associated with the request could not be found.",
+      "error_code": "Not_found",
+      "error_message": "The data you requested could not be found in Database"
+    })
+  }
+}
+
+var partialupdateUser = async (req: any, res: any) => {
+  var now = new Date()
+  var id = parseInt(req.params.id)
+  var recExists = await pool.query(`select * from datasets where id = '${id}'`)
+  if (recExists.rows.length != 0) {
+    var data_schema:object= req.body.data_schema||recExists.rows[0].data_schema
+  var router_config:object=req.body.router_config||recExists.rows[0].router_config
+  var status:string= req.body.status|| recExists.rows[0].status
+  var updated_by:string=req.body.updated_by|| recExists.rows[0].updated_date
+  // if(req.body.updated_by==null){
+  //   updated_by=recExists.rows[0].updated_by
+  // }
+    pool.query(`UPDATE datasets set data_schema=$1,router_config=$2,status=$3,updated_by=$4,updated_date=$5 where id =$6 ;`,
+      [data_schema, router_config, status, updated_by, now, id], (error: any, result: any) => {
+      if (!error) {
+        res.status(200).send({
+          "status_code": 200,
+          "success_message": "The record is successfully updated"
+        })
+      }
+      else {
+        res.status(400).send({
+          "status_code": 400,
+          "error_code": "Bad Request",
+          "error_message": "Invalid datatype"
+        })
+      }
+    })
+  }
+  else {
+    res.status(404).send({
+      "status_code": 404,
+      "reason_phrase": "The requested operation failed because a resource associated with the request could not be found.",
+      "error_code": "Not_found",
+      "error_message": "The data you requested could not be found in Database"
+    })
+  }
+}
 
 
 
@@ -130,23 +185,24 @@ var deleteUser = async (req: Request, res: Response) => {
   var id = parseInt(req.params.id)
   var recExists = await pool.query(`select * from datasets where id = '${id}'`)
   if (recExists.rows.length != 0) {
-  var gclient = await pool.query(' delete from datasets where id = $1;', [id], (error: any, results: any) => {
-    if (!error) {
+    var gclient = await pool.query(' delete from datasets where id = $1;', [id], (error: any, results: any) => {
+      if (!error) {
         res.status(200).send({
-          "status_code":200,
-          "success_message":"The record is deleted"
+          "status_code": 200,
+          "success_message": "The record is deleted"
+        })
+
+      }
     })
-    
-    }
-  })}
-  else{
+  }
+  else {
     res.status(404).send({
       "status_code": 404,
       "reason_phrase": "The requested operation failed because a resource associated with the request could not be found.",
       "error_code": "Not_found",
       "error_message": "The data you requested could not be found in Database"
-  })
-}
+    })
+  }
 }
 
 // module.exports = {
@@ -160,8 +216,9 @@ var deleteUser = async (req: Request, res: Response) => {
 
 export {
   getUsers,
-   getUserById,
-   createUser,
-   updateUser,
-   deleteUser
+  getUserById,
+  createUser,
+  updateUser,
+  partialupdateUser,
+  deleteUser
 }
